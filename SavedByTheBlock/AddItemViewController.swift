173b997df7a360391858faf87ff164b961f65a3e
Ashley Davis <85644278+adaviz@users.noreply.github.com>
2022-11-29 23:41:01 -0500

    Add files via upload

---
+++ SavedByTheBlock/AddItemViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddItemViewController.swift
+//  SavedByTheBlock
+//
+//  Created by Ashley Davis on 11/29/22.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func submit(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SavedByTheBlock/AppDelegate.swift
+++ SavedByTheBlock/AppDelegate.swift
@@ -7,7 +7,8 @@
 import Parse
 import UIKit
 
- @main
+
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
